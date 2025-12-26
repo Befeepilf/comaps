03c6f52a1338eac7e49bb3d8a49b6012718f584f
Befeepilf <befeepilf@protonmail.com>
2025-12-26 23:52:14 +0100

    Store by geometry hash which tracks have already been processed + remove explored fractions by track

--- libs/map/street_stats_db.cpp
+++ libs/map/street_stats_db.cpp
@@ -1,5 +1,8 @@
 #include "map/street_stats_db.hpp"
 
+#include "indexer/mwm_set.hpp"
+#include "storage/storage_defines.hpp"
+
 #include "platform/platform.hpp"
 
 #include "coding/file_writer.hpp"
@@ -10,6 +13,7 @@
 
 #include <sqlite3.h>
 
+#include <cstdint>
 #include <mutex>
 
 namespace
@@ -26,12 +30,11 @@ int64_t GetMwmId(sqlite3 * db, MwmSet::MwmId const & mwmId)
   auto const & mwmName = mwmInfo->GetCountryName();
 
   sqlite3_stmt * stmt = nullptr;
-  SCOPE_GUARD(finalize,
-              [stmt]()
-              {
-                if (stmt)
-                  sqlite3_finalize(stmt);
-              });
+  SCOPE_GUARD(finalize, [stmt]()
+  {
+    if (stmt)
+      sqlite3_finalize(stmt);
+  });
 
   char const * sql = "SELECT mwm_id FROM mwms WHERE mwm_name = ?;";
   if (sqlite3_prepare_v2(db, sql, -1, &stmt, nullptr) == SQLITE_OK)
@@ -69,6 +72,7 @@ StreetStatsDB & StreetStatsDB::Instance()
 
 StreetStatsDB::StreetStatsDB()
 {
+  LOG(LINFO, ("Initializing street stats database..."));
   m_dbPath = GetPlatform().WritablePathForFile(kDatabaseFileName);
   if (sqlite3_open_v2(m_dbPath.c_str(), &m_db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, nullptr) != SQLITE_OK)
   {
@@ -78,6 +82,7 @@ StreetStatsDB::StreetStatsDB()
     return;
   }
   InitSchema();
+  LOG(LINFO, ("Street stats database initialized successfully"));
 }
 
 StreetStatsDB::~StreetStatsDB()
@@ -88,19 +93,23 @@ StreetStatsDB::~StreetStatsDB()
 
 void StreetStatsDB::InitSchema()
 {
-  if (!m_db)
-    return;
-
+  ASSERT(m_db, ("Street stats database is not initialized"));
   char * errMsg = nullptr;
   char const * sql =
-    "CREATE TABLE IF NOT EXISTS mwms ("
-    "mwm_id INTEGER PRIMARY KEY, "
-    "mwm_name TEXT UNIQUE NOT NULL);"
-    "CREATE TABLE IF NOT EXISTS street_exploration ("
-    "mwm_id INTEGER, "
-    "feature_index INTEGER, "
-    "pixel_bitmask BLOB, "
-    "PRIMARY KEY (mwm_id, feature_index));";
+      "CREATE TABLE IF NOT EXISTS mwms ("
+      "mwm_id INTEGER PRIMARY KEY, "
+      "mwm_name TEXT UNIQUE NOT NULL);"
+      //
+      "CREATE TABLE IF NOT EXISTS street_exploration ("
+      "mwm_id INTEGER, "
+      "feature_index INTEGER, "
+      "pixel_bitmask BLOB, "
+      "PRIMARY KEY (mwm_id, feature_index));"
+      //
+      "CREATE TABLE IF NOT EXISTS processed_tracks ("
+      "geometry_hash INTEGER, "
+      "country_id TEXT, "
+      "PRIMARY KEY (geometry_hash, country_id));";
 
   if (sqlite3_exec(m_db, sql, nullptr, nullptr, &errMsg) != SQLITE_OK)
   {
@@ -111,20 +120,19 @@ void StreetStatsDB::InitSchema()
 
 void StreetStatsDB::BeginTransaction()
 {
-  if (m_db)
-    sqlite3_exec(m_db, "BEGIN TRANSACTION;", nullptr, nullptr, nullptr);
+  ASSERT(m_db, ("Street stats database is not initialized"));
+  sqlite3_exec(m_db, "BEGIN TRANSACTION;", nullptr, nullptr, nullptr);
 }
 
 void StreetStatsDB::EndTransaction()
 {
-  if (m_db)
-    sqlite3_exec(m_db, "COMMIT;", nullptr, nullptr, nullptr);
+  ASSERT(m_db, ("Street stats database is not initialized"));
+  sqlite3_exec(m_db, "COMMIT;", nullptr, nullptr, nullptr);
 }
 
 std::optional<StreetStatsDB::Bitmask> StreetStatsDB::GetBitmask(MwmSet::MwmId const & mwmId, uint32_t featureId)
 {
-  if (!m_db)
-    return {};
+  ASSERT(m_db, ("Street stats database is not initialized"));
 
   std::lock_guard<std::recursive_mutex> lock(m_mutex);
 
@@ -133,12 +141,11 @@ std::optional<StreetStatsDB::Bitmask> StreetStatsDB::GetBitmask(MwmSet::MwmId co
     return {};
 
   sqlite3_stmt * stmt = nullptr;
-  SCOPE_GUARD(finalize,
-              [stmt]()
-              {
-                if (stmt)
-                  sqlite3_finalize(stmt);
-              });
+  SCOPE_GUARD(finalize, [stmt]()
+  {
+    if (stmt)
+      sqlite3_finalize(stmt);
+  });
   char const * sql = "SELECT pixel_bitmask FROM street_exploration WHERE mwm_id = ? AND feature_index = ?;";
 
   if (sqlite3_prepare_v2(m_db, sql, -1, &stmt, nullptr) != SQLITE_OK)
@@ -164,8 +171,7 @@ std::optional<StreetStatsDB::Bitmask> StreetStatsDB::GetBitmask(MwmSet::MwmId co
 
 void StreetStatsDB::SaveBitmask(MwmSet::MwmId const & mwmId, uint32_t featureId, Bitmask const & bitmask)
 {
-  if (!m_db)
-    return;
+  ASSERT(m_db, ("Street stats database is not initialized"));
 
   std::lock_guard<std::recursive_mutex> lock(m_mutex);
 
@@ -174,15 +180,14 @@ void StreetStatsDB::SaveBitmask(MwmSet::MwmId const & mwmId, uint32_t featureId,
     return;
 
   sqlite3_stmt * stmt = nullptr;
-  SCOPE_GUARD(finalize,
-              [stmt]()
-              {
-                if (stmt)
-                  sqlite3_finalize(stmt);
-              });
+  SCOPE_GUARD(finalize, [stmt]()
+  {
+    if (stmt)
+      sqlite3_finalize(stmt);
+  });
 
   char const * sql =
-    "INSERT OR REPLACE INTO street_exploration (mwm_id, feature_index, pixel_bitmask) VALUES (?, ?, ?);";
+      "INSERT OR REPLACE INTO street_exploration (mwm_id, feature_index, pixel_bitmask) VALUES (?, ?, ?);";
 
   if (sqlite3_prepare_v2(m_db, sql, -1, &stmt, nullptr) != SQLITE_OK)
   {
@@ -195,25 +200,21 @@ void StreetStatsDB::SaveBitmask(MwmSet::MwmId const & mwmId, uint32_t featureId,
   sqlite3_bind_blob(stmt, 3, bitmask.data(), bitmask.size(), SQLITE_TRANSIENT);
 
   if (sqlite3_step(stmt) != SQLITE_DONE)
-  {
     LOG(LERROR, ("Failed to save bitmask:", sqlite3_errmsg(m_db)));
-  }
 }
 
 void StreetStatsDB::DeleteMwmData(std::string const & mwmName)
 {
-  if (!m_db)
-    return;
+  ASSERT(m_db, ("Street stats database is not initialized"));
 
   std::lock_guard<std::recursive_mutex> lock(m_mutex);
 
   sqlite3_stmt * stmt = nullptr;
-  SCOPE_GUARD(finalize,
-              [stmt]()
-              {
-                if (stmt)
-                  sqlite3_finalize(stmt);
-              });
+  SCOPE_GUARD(finalize, [stmt]()
+  {
+    if (stmt)
+      sqlite3_finalize(stmt);
+  });
 
   int64_t internalMwmId = -1;
   char const * sqlSelect = "SELECT mwm_id FROM mwms WHERE mwm_name = ?;";
@@ -244,6 +245,50 @@ void StreetStatsDB::DeleteMwmData(std::string const & mwmName)
     sqlite3_bind_int64(stmt, 1, internalMwmId);
     sqlite3_step(stmt);
   }
+}
+
+bool StreetStatsDB::IsTrackProcessed(std::int64_t const geometryHash, storage::CountryId const & countryId)
+{
+  ASSERT(m_db, ("Street stats database is not initialized"));
+
+  std::lock_guard<std::recursive_mutex> lock(m_mutex);
+
+  sqlite3_stmt * stmt = nullptr;
+  SCOPE_GUARD(finalize, [stmt]()
+  {
+    if (stmt)
+      sqlite3_finalize(stmt);
+  });
+
+  char const * sql = "SELECT 1 FROM processed_tracks WHERE geometry_hash = ? AND country_id = ?;";
+  if (sqlite3_prepare_v2(m_db, sql, -1, &stmt, nullptr) != SQLITE_OK)
+    return false;
+
+  sqlite3_bind_int64(stmt, 1, geometryHash);
+  sqlite3_bind_text(stmt, 2, countryId.c_str(), -1, SQLITE_TRANSIENT);
+  return sqlite3_step(stmt) == SQLITE_ROW;
+}
+
+void StreetStatsDB::MarkTrackProcessed(std::int64_t geometryHash, storage::CountryId const & countryId)
+{
+  ASSERT(m_db, ("Street stats database is not initialized"));
+
+  std::lock_guard<std::recursive_mutex> lock(m_mutex);
+
+  sqlite3_stmt * stmt = nullptr;
+  SCOPE_GUARD(finalize, [stmt]()
+  {
+    if (stmt)
+      sqlite3_finalize(stmt);
+  });
+
+  char const * sql = "INSERT OR IGNORE INTO processed_tracks (geometry_hash, country_id) VALUES (?, ?);";
+  if (sqlite3_prepare_v2(m_db, sql, -1, &stmt, nullptr) != SQLITE_OK)
+    return;
+
+  sqlite3_bind_int64(stmt, 1, geometryHash);
+  sqlite3_bind_text(stmt, 2, countryId.c_str(), -1, SQLITE_TRANSIENT);
+  sqlite3_step(stmt);
 }
 
 }  // namespace street_stats
